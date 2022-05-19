faf9c941f20271204c6e1c7d396984e66997b316
tetsuya <tetsuya>
2022-05-20 01:46:00 +0900

    The values are explicitly expressed as float.

--- D3Q19_opencl/initialization.hpp
+++ D3Q19_opencl/initialization.hpp
@@ -8,14 +8,14 @@ for(int k = 0; k < nz; k++)
             int ic = index1d(i,j,k,nx,ny);
         
             float rho = rho0;
-            float u = 0.0;
-            float v = 0.0;
-            float w = 0.0;
+            float u = 0.0f;
+            float v = 0.0f;
+            float w = 0.0f;
             for(int q = 0; q < 19; q++)
             {
                 float uSqr =u*u+v*v+w*w;
                 float uDotC = u*cx[q]+v*cy[q]+w*cz[q];
-                float feq = (1.0+3.0*uDotC +4.5*uDotC*uDotC -1.5*uSqr)*wt[q]*rho;
+                float feq = (1.0f+3.0f*uDotC +4.5f*uDotC*uDotC -1.5f*uSqr)*wt[q]*rho;
 
                 int icf = idf(q,ic,nx,ny,nz);
                 f[icf] = feq;
