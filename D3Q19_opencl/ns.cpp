38ea85fd6c03e69add22ed22d82cfa2f09f03bff
tetsuya <tetsuya>
2022-06-19 00:10:06 +0900

    Change the size of calculation region for flow around cylinder

--- D3Q19_opencl/ns.cpp
+++ D3Q19_opencl/ns.cpp
@@ -76,8 +76,9 @@ int main()
     
 
     //- For flow around cylinder
-    float h = 6.f;
-    float d = 6.f;
+    float h = 1.f;
+    // float d = 6.f;
+    float d = 0.1f;
     float nu = uMax*d/Re;
     const float L = h/float(ny);
     float dpdx = 0.f;
