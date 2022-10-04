4e136a33444c89d2f925ff127e5086f82c764bee
tetsuya <tetsuya>
2022-10-04 20:23:36 +0900

    Fix setting of boundary conditions

--- D3Q19_opencl/setBoundaryConditions.hpp
+++ D3Q19_opencl/setBoundaryConditions.hpp
@@ -55,79 +55,79 @@ for(int k = 0; k < nz; k++)
 
             if(i == 0 && j == 0)
             {
-                u0[ic] = fmax(nxMinU0, nyMinU0);
-                v0[ic] = fmax(nxMinV0, nyMinV0);
-                w0[ic] = fmax(nxMinW0, nyMinW0);
+                u0[ic] = (nxMinU0*nyMinU0 == 0.f) ? 0.f : nxMinU0;
+                v0[ic] = (nxMinV0*nyMinV0 == 0.f) ? 0.f : nxMinV0;
+                w0[ic] = (nxMinW0*nyMinW0 == 0.f) ? 0.f : nxMinW0;
             }
             if(i == 0 && j == ny-1)
             {
-                u0[ic] = fmax(nxMinU0, nyMaxU0);
-                v0[ic] = fmax(nxMinV0, nyMaxV0);
-                w0[ic] = fmax(nxMinW0, nyMaxW0);
+                u0[ic] = (nxMinU0*nyMaxU0 == 0.f) ? 0.f : nxMinU0;
+                v0[ic] = (nxMinV0*nyMaxV0 == 0.f) ? 0.f : nxMinV0;
+                w0[ic] = (nxMinW0*nyMaxW0 == 0.f) ? 0.f : nxMinW0;
             }
             if(i == nx-1 && j == 0)
             {
-                u0[ic] = fmax(nxMaxU0, nyMinU0);
-                v0[ic] = fmax(nxMaxV0, nyMinV0);
-                w0[ic] = fmax(nxMaxW0, nyMinW0);
+                u0[ic] = (nxMaxU0*nyMinU0 == 0.f) ? 0.f : nxMaxU0;
+                v0[ic] = (nxMaxV0*nyMinV0 == 0.f) ? 0.f : nxMaxV0;
+                w0[ic] = (nxMaxW0*nyMinW0 == 0.f) ? 0.f : nxMaxW0;
             }
             if(i == nx-1 && j == ny-1)
             {
-                u0[ic] = fmax(nxMaxU0, nyMaxU0);
-                v0[ic] = fmax(nxMaxV0, nyMaxV0);
-                w0[ic] = fmax(nxMaxW0, nyMaxW0);
+                u0[ic] = (nxMaxU0*nyMaxU0 == 0.f) ? 0.f : nxMaxU0;
+                v0[ic] = (nxMaxV0*nyMaxV0 == 0.f) ? 0.f : nxMaxV0;
+                w0[ic] = (nxMaxW0*nyMaxW0 == 0.f) ? 0.f : nxMaxW0;
             }
 
 
             if(i == 0 && k == 0)
             {
-                u0[ic] = fmax(nxMinU0, nzMinU0);
-                v0[ic] = fmax(nxMinV0, nzMinV0);
-                w0[ic] = fmax(nxMinW0, nzMinW0);
+                u0[ic] = (nz-1 != 0 && nxMinU0*nzMinU0 == 0.f) ? 0.f : nxMinU0;
+                v0[ic] = (nz-1 != 0 && nxMinV0*nzMinV0 == 0.f) ? 0.f : nxMinV0;
+                w0[ic] = (nz-1 != 0 && nxMinW0*nzMinW0 == 0.f) ? 0.f : nxMinW0;
             }
             if(i == 0 && k == nz-1)
             {
-                u0[ic] = fmax(nxMinU0, nzMaxU0);
-                v0[ic] = fmax(nxMinV0, nzMaxV0);
-                w0[ic] = fmax(nxMinW0, nzMaxW0);
+                u0[ic] = (nz-1 != 0 && nxMinU0*nzMaxU0 == 0.f) ? 0.f : nxMinU0;
+                v0[ic] = (nz-1 != 0 && nxMinV0*nzMaxV0 == 0.f) ? 0.f : nxMinV0;
+                w0[ic] = (nz-1 != 0 && nxMinW0*nzMaxW0 == 0.f) ? 0.f : nxMinW0;
             }
             if(i == nx-1 && k == 0)
             {
-                u0[ic] = fmax(nxMaxU0, nzMinU0);
-                v0[ic] = fmax(nxMaxV0, nzMinV0);
-                w0[ic] = fmax(nxMaxW0, nzMinW0);
+                u0[ic] = (nz-1 != 0 && nxMaxU0*nzMinU0 == 0.f) ? 0.f : nxMaxU0;
+                v0[ic] = (nz-1 != 0 && nxMaxV0*nzMinV0 == 0.f) ? 0.f : nxMaxV0;
+                w0[ic] = (nz-1 != 0 && nxMaxW0*nzMinW0 == 0.f) ? 0.f : nxMaxW0;
             }
             if(i == nx-1 && k == nz-1)
             {
-                u0[ic] = fmax(nxMaxU0, nzMaxU0);
-                v0[ic] = fmax(nxMaxV0, nzMaxV0);
-                w0[ic] = fmax(nxMaxW0, nzMaxW0);
+                u0[ic] = (nz-1 != 0 && nxMaxU0*nzMaxU0 == 0.f) ? 0.f : nxMaxU0;
+                v0[ic] = (nz-1 != 0 && nxMaxV0*nzMaxV0 == 0.f) ? 0.f : nxMaxV0;
+                w0[ic] = (nz-1 != 0 && nxMaxW0*nzMaxW0 == 0.f) ? 0.f : nxMaxW0;
             }
             
 
             if(j == 0 && k == 0)
             {
-                u0[ic] = fmax(nyMinU0, nzMinU0);
-                v0[ic] = fmax(nyMinV0, nzMinV0);
-                w0[ic] = fmax(nyMinW0, nzMinW0);
+                u0[ic] = (nz-1 != 0 && nyMinU0*nzMinU0 == 0.f) ? 0.f : nyMinU0;
+                v0[ic] = (nz-1 != 0 && nyMinV0*nzMinV0 == 0.f) ? 0.f : nyMinV0;
+                w0[ic] = (nz-1 != 0 && nyMinW0*nzMinW0 == 0.f) ? 0.f : nyMinW0;
             }
             if(j == 0 && k == nz-1)
             {
-                u0[ic] = fmax(nyMinU0, nzMaxU0);
-                v0[ic] = fmax(nyMinV0, nzMaxV0);
-                w0[ic] = fmax(nyMinW0, nzMaxW0);
+                u0[ic] = (nz-1 != 0 && nyMinU0*nzMaxU0 == 0.f) ? 0.f : nyMinU0;
+                v0[ic] = (nz-1 != 0 && nyMinV0*nzMaxV0 == 0.f) ? 0.f : nyMinV0;
+                w0[ic] = (nz-1 != 0 && nyMinW0*nzMaxW0 == 0.f) ? 0.f : nyMinW0;
             }
             if(j == ny-1 && k == 0)
             {
-                u0[ic] = fmax(nyMaxU0, nzMinU0);
-                v0[ic] = fmax(nyMaxV0, nzMinV0);
-                w0[ic] = fmax(nyMaxW0, nzMinW0);
+                u0[ic] = (nz-1 != 0 && nyMaxU0*nzMinU0 == 0.f) ? 0.f : nyMaxU0;
+                v0[ic] = (nz-1 != 0 && nyMaxV0*nzMinV0 == 0.f) ? 0.f : nyMaxV0;
+                w0[ic] = (nz-1 != 0 && nyMaxW0*nzMinW0 == 0.f) ? 0.f : nyMaxW0;
             }
             if(j == ny-1 && k == nz-1)
             {
-                u0[ic] = fmax(nyMaxU0, nzMaxU0);
-                v0[ic] = fmax(nyMaxV0, nzMaxV0);
-                w0[ic] = fmax(nyMaxW0, nzMaxW0);
+                u0[ic] = (nz-1 != 0 && nyMaxU0*nzMaxU0 == 0.f) ? 0.f : nyMaxU0;
+                v0[ic] = (nz-1 != 0 && nyMaxV0*nzMaxV0 == 0.f) ? 0.f : nyMaxV0;
+                w0[ic] = (nz-1 != 0 && nyMaxW0*nzMaxW0 == 0.f) ? 0.f : nyMaxW0;
             }            
 
 
@@ -135,51 +135,51 @@ for(int k = 0; k < nz; k++)
 
             if(i == 0 && j == 0 && k == 0)
             {
-                u0[ic] = fmax(fmax(nxMinU0, nyMinU0),nzMinU0);
-                v0[ic] = fmax(fmax(nxMinV0, nyMinV0),nzMinV0);
-                w0[ic] = fmax(fmax(nxMinW0, nyMinW0),nzMinW0);
+                u0[ic] = (nz-1 != 0 && nxMinU0*nyMinU0*nzMinU0 == 0.f) ? 0.f : nxMinU0;
+                v0[ic] = (nz-1 != 0 && nxMinV0*nyMinV0*nzMinV0 == 0.f) ? 0.f : nxMinV0;
+                w0[ic] = (nz-1 != 0 && nxMinW0*nyMinW0*nzMinW0 == 0.f) ? 0.f : nxMinW0;
             }
             if(i == nx-1 && j == 0 && k == 0)
             {
-                u0[ic] = fmax(fmax(nxMaxU0, nyMinU0),nzMinU0);
-                v0[ic] = fmax(fmax(nxMaxV0, nyMinV0),nzMinV0);
-                w0[ic] = fmax(fmax(nxMaxW0, nyMinW0),nzMinW0);
+                u0[ic] = (nz-1 != 0 && nxMaxU0*nyMinU0*nzMinU0 == 0.f) ? 0.f : nxMaxU0;
+                v0[ic] = (nz-1 != 0 && nxMaxV0*nyMinV0*nzMinV0 == 0.f) ? 0.f : nxMaxV0;
+                w0[ic] = (nz-1 != 0 && nxMaxW0*nyMinW0*nzMinW0 == 0.f) ? 0.f : nxMaxW0;
             }
             if(i == 0 && j == ny-1 && k == 0)
             {
-                u0[ic] = fmax(fmax(nxMinU0, nyMaxU0),nzMinU0);
-                v0[ic] = fmax(fmax(nxMinV0, nyMaxV0),nzMinV0);
-                w0[ic] = fmax(fmax(nxMinW0, nyMaxW0),nzMinW0);
+                u0[ic] = (nz-1 != 0 && nxMinU0*nyMaxU0*nzMinU0 == 0.f) ? 0.f : nxMinU0;
+                v0[ic] = (nz-1 != 0 && nxMinV0*nyMaxV0*nzMinV0 == 0.f) ? 0.f : nxMinV0;
+                w0[ic] = (nz-1 != 0 && nxMinW0*nyMaxW0*nzMinW0 == 0.f) ? 0.f : nxMinW0;
             }
             if(i == 0 && j == 0 && k == nz-1)
             {
-                u0[ic] = fmax(fmax(nxMinU0, nyMinU0),nzMaxU0);
-                v0[ic] = fmax(fmax(nxMinV0, nyMinV0),nzMaxV0);
-                w0[ic] = fmax(fmax(nxMinW0, nyMinW0),nzMaxW0);
+                u0[ic] = (nz-1 != 0 && nxMinU0*nyMinU0*nzMaxU0 == 0.f) ? 0.f : nxMinU0;
+                v0[ic] = (nz-1 != 0 && nxMinV0*nyMinV0*nzMaxV0 == 0.f) ? 0.f : nxMinV0;
+                w0[ic] = (nz-1 != 0 && nxMinW0*nyMinW0*nzMaxW0 == 0.f) ? 0.f : nxMinW0;
             }
             if(i == nx-1 && j == ny-1 && k == 0)
             {
-                u0[ic] = fmax(fmax(nxMaxU0, nyMaxU0),nzMinU0);
-                v0[ic] = fmax(fmax(nxMaxV0, nyMaxV0),nzMinV0);
-                w0[ic] = fmax(fmax(nxMaxW0, nyMaxW0),nzMinW0);
+                u0[ic] = (nz-1 != 0 && nxMaxU0*nyMaxU0*nzMinU0 == 0.f) ? 0.f : nxMaxU0;
+                v0[ic] = (nz-1 != 0 && nxMaxV0*nyMaxV0*nzMinV0 == 0.f) ? 0.f : nxMaxV0;
+                w0[ic] = (nz-1 != 0 && nxMaxW0*nyMaxW0*nzMinW0 == 0.f) ? 0.f : nxMaxW0;
             }
             if(i == 0 && j == ny-1 && k == nz-1)
             {
-                u0[ic] = fmax(fmax(nxMinU0, nyMaxU0),nzMaxU0);
-                v0[ic] = fmax(fmax(nxMinV0, nyMaxV0),nzMaxV0);
-                w0[ic] = fmax(fmax(nxMinW0, nyMaxW0),nzMaxW0);
+                u0[ic] = (nz-1 != 0 && nxMinU0*nyMaxU0*nzMaxU0 == 0.f) ? 0.f : nxMinU0;
+                v0[ic] = (nz-1 != 0 && nxMinV0*nyMaxV0*nzMaxV0 == 0.f) ? 0.f : nxMinV0;
+                w0[ic] = (nz-1 != 0 && nxMinW0*nyMaxW0*nzMaxW0 == 0.f) ? 0.f : nxMinW0;
             }
             if(i == nx-1 && j == 0 && k == nz-1)
             {
-                u0[ic] = fmax(fmax(nxMaxU0, nyMinU0),nzMaxU0);
-                v0[ic] = fmax(fmax(nxMaxV0, nyMinV0),nzMaxV0);
-                w0[ic] = fmax(fmax(nxMaxW0, nyMinW0),nzMaxW0);
+                u0[ic] = (nz-1 != 0 && nxMaxU0*nyMinU0*nzMaxU0 == 0.f) ? 0.f : nxMaxU0;
+                v0[ic] = (nz-1 != 0 && nxMaxV0*nyMinV0*nzMaxV0 == 0.f) ? 0.f : nxMaxV0;
+                w0[ic] = (nz-1 != 0 && nxMaxW0*nyMinW0*nzMaxW0 == 0.f) ? 0.f : nxMaxW0;
             }
             if(i == nx-1 && j == ny-1 && k == nz-1)
             {
-                u0[ic] = fmax(fmax(nxMaxU0, nyMaxU0),nzMaxU0);
-                v0[ic] = fmax(fmax(nxMaxV0, nyMaxV0),nzMaxV0);
-                w0[ic] = fmax(fmax(nxMaxW0, nyMaxW0),nzMaxW0);
+                u0[ic] = (nz-1 != 0 && nxMaxU0*nyMaxU0*nzMaxU0 == 0.f) ? 0.f : nxMaxU0;
+                v0[ic] = (nz-1 != 0 && nxMaxV0*nyMaxV0*nzMaxV0 == 0.f) ? 0.f : nxMaxV0;
+                w0[ic] = (nz-1 != 0 && nxMaxW0*nyMaxW0*nzMaxW0 == 0.f) ? 0.f : nxMaxW0;
             }
         }
     }
