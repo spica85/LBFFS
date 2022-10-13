d846900905a479d3e400ec38cbb4e0dc4c5388ac
tetsuya <tetsuya>
2022-10-13 19:53:53 +0900

    Fix the settings so that are compatible to the changes in BCs

--- D3Q19_opencl/testCases/flowAroundCylinder/post.sh
+++ D3Q19_opencl/testCases/flowAroundCylinder/post.sh
@@ -4,5 +4,3 @@ grep Cy: log | awk '{print $4}' | sed 's/,//g' > Cy.dat
 grep Cx: log | awk '{print $2}' | sed 's/,//g' > Cx.dat
 paste time.dat Cx.dat > t_Cx.dat
 paste time.dat Cy.dat > t_Cy.dat
-sed '1s/^/t\ty\n/' t_Cx.dat > tmp; mv tmp t_Cx
-sed '1s/^/t\ty\n/' t_Cy.dat > tmp; mv tmp t_Cy
--- D3Q19_opencl/testCases/flowAroundCylinder/run.sh
+++ D3Q19_opencl/testCases/flowAroundCylinder/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cp input*.txt ../../build/input.txt
 cp boundaryConditions*.txt ../../build/boundaryConditions.txt
-cp cylinder.stl ../../build/
+cp cylinder.stl outlets.stl ../../build/
 cd ../../build/
-rm -rf walls.stl movingWalls.stl inlets.stl outlets.stl
+rm -rf walls.stl movingWalls.stl inlets.stl
 ln -s cylinder.stl walls.stl
 ./ns.opencl | tee log
