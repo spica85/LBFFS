7b55dda1ed868fe11e0ab42363a73e579a95b859
tetsuya <tetsuya>
2022-10-27 15:56:54 +0900

    Fix

--- D3Q19_opencl/testCases/flowAroundCylinder/run.sh
+++ D3Q19_opencl/testCases/flowAroundCylinder/run.sh
@@ -3,6 +3,6 @@ cp input*.txt ../../build/input.txt
 cp boundaryConditions*.txt ../../build/boundaryConditions.txt
 cp cylinder.stl ../../build/
 cd ../../build/
-rm -rf walls.stl movingWalls.stl inlets.stl
+rm -rf walls.stl movingWalls.stl inlets.stl outlets.stl
 ln -s cylinder.stl walls.stl
 ./ns.opencl | tee log
